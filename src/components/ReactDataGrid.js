4f7ba3dae55152d7833ef80a30c4f016bb41d530
Paulo Henrique <phvcandido@gmail.com>
2021-10-23 12:29:05 -0300

    ✨feat: Adding ReactDataGridUsage intellisense

--- src/components/ReactDataGrid.js
+++ src/components/ReactDataGrid.js
@@ -1,31 +1,17 @@
-import React, { useState, useCallback } from "react";
+/* eslint react/prop-types: "off" */
+import PropTypes from "prop-types";
+import React, { useCallback } from "react";
 
 import ReactDataGrid from "@inovua/reactdatagrid-enterprise";
 import "@inovua/reactdatagrid-enterprise/index.css";
 
-import tableData from "../source/tablesData.json";
-const columns = [
-  {
-    name: "id",
-    header: "Id",
-    defaultVisible: false,
-    minWidth: 300,
-    type: "number"
-  },
-  { name: "name", header: "Name", defaultFlex: 1, minWidth: 250 },
-  { name: "city", header: "City", defaultFlex: 1, minWidth: 300 },
-  { name: "age", header: "Age", minWidth: 150, type: "number" }
-];
-
-const ReactDataGridUsage = () => {
-  const [dataSource, setDataSource] = useState(tableData);
-
+const ReactDataGridUsage = ({ columns, dataSource, onUpdate }) => {
   const onEditComplete = useCallback(
     ({ value, columnId, rowIndex }) => {
       const data = [...dataSource];
       data[rowIndex][columnId] = value;
 
-      setDataSource(data);
+      onUpdate(data);
     },
     [dataSource]
   );
@@ -39,9 +25,12 @@ const ReactDataGridUsage = () => {
         columns={columns}
         dataSource={dataSource}
       />
-      <pre>{JSON.stringify(dataSource, null, 2)}</pre>
     </div>
   );
 };
 
-export default () => <ReactDataGridUsage />;
+ReactDataGridUsage.PropTypes = {
+  test: PropTypes.string.isRequired
+};
+
+export default ReactDataGridUsage;
--- src/pages/ReactDataGridUsage.js
+++ src/pages/ReactDataGridUsage.js
@@ -1,8 +1,32 @@
+/* eslint react/prop-types: "off" */
 import React from "react";
 import ReactDataGrid from "../components/ReactDataGrid";
+import tableData from "../source/tablesData.json";
 
 const ReactDataGridUsage = () => {
-  return <ReactDataGrid />;
+  const [dataSource, setDataSource] = React.useState(tableData);
+
+  return (
+    <>
+      <ReactDataGrid
+        columns={[
+          {
+            name: "id",
+            header: "Id",
+            defaultVisible: false,
+            minWidth: 300,
+            type: "number"
+          },
+          { name: "name", header: "Name", defaultFlex: 1, minWidth: 250 },
+          { name: "city", header: "City", defaultFlex: 1, minWidth: 300 },
+          { name: "age", header: "Age", minWidth: 150, type: "number" }
+        ]}
+        dataSource={dataSource}
+        onUpdate={setDataSource}
+      />
+      <pre>{JSON.stringify(dataSource, null, 2)}</pre>
+    </>
+  );
 };
 
 export default ReactDataGridUsage;
