302ad895cda1f5594fe3d43c8d32c710221173b5
Paulo Henrique <phvcandido@gmail.com>
2021-10-23 09:32:12 -0300

    Initial commit

---
+++ src/components/DataTable.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DataTable = () => {
+  return "Here goes the table";
+};
+
+export default DataTable;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return "Here is the home";
+};
+
+export default Home;
