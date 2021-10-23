81055ae6126fad562a03ac1d6650f2845c90c7a4
Paulo Henrique <phvcandido@gmail.com>
2021-10-23 10:06:29 -0300

    :hammer: feat: Adding the json schemes

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,52 +1,17 @@
 import React from "react";
 import { FormRenderer } from "@data-driven-forms/react-form-renderer";
-import componentTypes from "@data-driven-forms/react-form-renderer/component-types";
 import { Typography } from "@material-ui/core";
 import FormTemplate from "../components/FormTemplate";
 import PersonalizedTextField from "../components/PersonalizedTextField";
+import schema from "../schemes/firstFormExample.json";
 
 const componentMapper = {
-  [componentTypes.TEXT_FIELD]: PersonalizedTextField,
+  textField: PersonalizedTextField,
   "custom-component-type": PersonalizedTextField
 };
 
 const ComponentMapper = () => {
   const [values, setValues] = React.useState({});
-  const schema = {
-    fields: [
-      {
-        component: componentTypes.TEXT_FIELD,
-        name: "first-name",
-        label: "First name",
-        isRequired: true,
-        validate: [
-          (value) => (!value || value.lenght === 0 ? "Required" : undefined)
-        ],
-        customProp: true
-      },
-      {
-        component: componentTypes.TEXT_FIELD,
-        name: "last-name",
-        label: "Last name",
-        isRequired: true,
-        validate: [
-          (value) => (!value || value.lenght === 0 ? "Required" : undefined)
-        ]
-      },
-      {
-        component: componentTypes.TEXT_FIELD,
-        name: "age",
-        label: "Your age",
-        type: "number"
-      },
-      {
-        component: "custom-component-type",
-        name: "password",
-        label: "Your password",
-        type: "password"
-      }
-    ]
-  };
   return (
     <div>
       <Typography variant="h4">Formulário com inputs personalizados</Typography>
